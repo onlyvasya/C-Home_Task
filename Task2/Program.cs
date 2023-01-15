934e49b2d944a0c121c1c1ed037447b90b2552e0
onlyvasya <onlyvasya@mail.ru>
2023-01-15 17:39:08 +0200

    Добавлена Задача2

---
+++ Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("ВВедите первое число");
+int a=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("ВВедите второе число");
+int b=Convert.ToInt32(Console.ReadLine());
+if (a>b)
+{
+    Console.WriteLine("Большее число  ");
+    Console.WriteLine(a);
+}
+else
+{
+    Console.WriteLine("Большее число  ");
+    Console.WriteLine(b);
+}
