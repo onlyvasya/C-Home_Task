1794366ebbdb01c5f0ce1e841fa3dfaac16754d1
onlyvasya <onlyvasya@mail.ru>
2023-01-16 11:06:04 +0200

    добавлена задача 4

---
+++ Task4/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("ВВедите первое число");
+int a=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("ВВедите второе число");
+int b=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("ВВедите третье число");
+int c=Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (b>max)
+{
+   max=b;
+}
+if (c>max)
+{
+    max=c;
+}
+Console.WriteLine("Максимальное число "+ max);
