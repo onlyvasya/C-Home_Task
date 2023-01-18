07bb9297979ca02b95a18898f360369f8d795e0a
onlyvasya <onlyvasya@mail.ru>
2023-01-18 22:06:12 +0200

    Добавлена задача 15

---
+++ Task15/Program.cs
@@ -0,0 +1,11 @@
+﻿System.Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a >7) 
+{
+    System.Console.WriteLine("Введите число от 1 до 7");
+}
+else if (a==6 || a==7)
+{
+    System.Console.WriteLine("да");
+}
+else System.Console.WriteLine("нет");
