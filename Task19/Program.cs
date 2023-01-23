0a4a274d013272cec4f1a2de69057ac136b601ee
onlyvasya <onlyvasya@mail.ru>
2023-01-23 13:35:44 +0200

    Добавлена задача 19

---
+++ Task19/Program.cs
@@ -0,0 +1,8 @@
+﻿System.Console.WriteLine("Введите пятизначное число");
+int a = Convert.ToInt32(Console.ReadLine());
+string str = a.ToString();
+if (str[0]==str[4]&&str[1]==str[3])
+{
+    System.Console.WriteLine("да");
+}
+else System.Console.WriteLine("нет");
