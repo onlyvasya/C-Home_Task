463a82e5a8598f93b628ceba5594e9259c44b48a
onlyvasya <onlyvasya@mail.ru>
2023-01-26 20:25:14 +0200

    Добавлена задача 25

---
+++ Task25/Program.cs
@@ -0,0 +1,20 @@
+﻿
+//Функцию написал еще для прошлого дз когда в куб возводили ;)
+
+int grade(int a, int b) 
+{
+int result;
+result =1;
+for (int i = 0; i < b; i++)
+{
+    result =  result * a;
+}
+return (result);
+}
+
+System.Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите второе  число");
+int b = Convert.ToInt32(Console.ReadLine());
+System.Console.Write(grade(a,b));
+    
