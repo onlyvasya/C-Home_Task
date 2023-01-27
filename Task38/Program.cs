1498efd935a3232b8fa5e14a8d1a37004d24f38e
onlyvasya <onlyvasya@mail.ru>
2023-01-27 20:20:51 +0200

    Добавлена задача 38

---
+++ Task38/Program.cs
@@ -0,0 +1,19 @@
+﻿System.Console.WriteLine("Введите размер массива");
+int n = Convert.ToInt32(Console.ReadLine());
+double[] array = new double[n];
+Random rnd = new Random();
+for (int i = 0; i < n; i++)
+{
+    array[i] = rnd.Next(1,10);
+    System.Console.Write(" " + array[i]);
+}
+double max=array[0];
+double min=array[0];
+foreach (var item in array)  // foreach попробовал
+{
+   if (item>max) max=item;
+   if (item<min) min=item;
+}
+double delta = max-min;
+System.Console.WriteLine();
+System.Console.WriteLine("Разница между максимальным и минимальным элементом массива равна "+delta);
