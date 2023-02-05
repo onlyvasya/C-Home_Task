0a1490242980b9803e19b1c61b14d9904505e485
onlyvasya <onlyvasya@mail.ru>
2023-02-06 01:51:21 +0200

    Добавлена задача 50

---
+++ Task50/Program.cs
@@ -0,0 +1,32 @@
+﻿double DoubleRnd(int a, int b)                            // функция на генерацию вещественных чисел в разных диапазонах
+{
+Random rnd = new Random();
+double doublernd = a+ rnd.NextDouble() *(b-a);
+return doublernd;
+}
+void FillPrintArray (double[,]arr)                        // Функция на заполнение и распечатку массива c вызовом функции генерации      
+{                                                         
+   for (int i = 0; i < arr.GetLength(0); i++)
+ {
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        arr[i,j] = Math.Round(DoubleRnd(-100,100),2);              
+        System.Console.Write(" "+ arr[i,j]+" ");
+    }
+    System.Console.WriteLine();    
+ }
+}
+System.Console.WriteLine("Введите количество строк");       
+int m = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите количество столбцов");
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] col = new double[m,n];
+FillPrintArray(col);
+System.Console.WriteLine("Введите номер строки");         
+int r = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите номер столбца");         
+int c = Convert.ToInt32(Console.ReadLine());
+if(r>col.GetLength(0) & c>col.GetLength(1)) System.Console.WriteLine("Такого элемента нет");
+else System.Console.WriteLine("-->"+ col[r,c]);
+
+
