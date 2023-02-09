43c483ed1f68957ef0e28b5cd8253a94135525e1
onlyvasya <onlyvasya@mail.ru>
2023-02-09 23:18:13 +0200

    Добавлена задача 62

---
+++ Task62/Program.cs
@@ -0,0 +1,53 @@
+﻿// Сделал тоже тупо в лоб, без функций и рекурсий, потому что их уже  у Дмитрия подсмотрел, не честно )
+
+
+void PrintArray(int[,] array)                        
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write("{0,6}", array[i, j] + " ");
+        }
+        System.Console.WriteLine();
+    }
+}
+int m = 4;
+int k = 4;
+int[,] arr = new int[m, k];
+int n = 1;
+for (int j = 0; j < k; j++)
+{
+    arr[m - m, j] = n;
+    n++;
+}
+for (int i = m - 3; i < m; i++)
+{
+    arr[i, k - 1] = n;
+    n++;
+}
+for (int j = k - 2; j >= 0; j--)
+{
+    arr[m - 1, j] = n;
+    n++;
+}
+for (int i = m - 2; i >= m - 3; i--)
+{
+    arr[i, k - k] = n;
+    n++;
+}
+for (int j = k - 3; j < k - 1; j++)
+{
+    arr[m - 3, j] = n;
+    n++;
+}
+for (int i = m - 2; i >= m - 2; i--)
+{
+    arr[i, k - 2] = n;
+    n++;
+}
+for (int j = k - 3; j > k - k; j--)
+{
+    arr[m - 2, j] = n;
+}
+PrintArray(arr);
